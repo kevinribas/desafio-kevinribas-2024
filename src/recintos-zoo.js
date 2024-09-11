4b93e8828a05529ce460fe2d59fbbac8109e972c
Kevin Ribas <50385593+kevinribas@users.noreply.github.com>
2024-09-10 21:46:48 -0300

    constructor implementation with existing animals and enclosures

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,5 +1,27 @@
 class RecintosZoo {
 
+    constructor() {
+
+        // Definindo os biomas e tamanhos para cada animal
+        this.animais = {
+            'LEAO': { tamanho: 3, bioma: ['savana'], carnivoro: true },
+            'LEOPARDO': { tamanho: 2, bioma: ['savana'], carnivoro: true },
+            'CROCODILO': { tamanho: 3, bioma: ['rio'], carnivoro: true },
+            'MACACO': { tamanho: 1, bioma: ['savana', 'floresta'], carnivoro: false },
+            'GAZELA': { tamanho: 2, bioma: ['savana'], carnivoro: false },
+            'HIPOPOTAMO': { tamanho: 4, bioma: ['savana', 'rio'], carnivoro: false }
+        };
+
+        // Recintos existentes
+        this.recintos = [
+            { numero: 1, bioma: 'savana', tamanho: 10, animais: [{ especie: 'MACACO', quantidade: 3 }] },
+            { numero: 2, bioma: 'floresta', tamanho: 5, animais: [] },
+            { numero: 3, bioma: 'savana e rio', tamanho: 7, animais: [{ especie: 'GAZELA', quantidade: 1 }] },
+            { numero: 4, bioma: 'rio', tamanho: 8, animais: [] },
+            { numero: 5, bioma: 'savana', tamanho: 9, animais: [{ especie: 'LEAO', quantidade: 1 }] }
+        ];
+    }
+
     analisaRecintos(animal, quantidade) {
     }
 
